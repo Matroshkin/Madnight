63daf7ed6f53506739950b96589049d83114a7ec
michael-slugin <slugin.m@i.ua>
2017-02-25 13:43:08 +0200

    slugin-some-global-staff
    controller, light, flashlight, door interact, some cosmetic changes for
    better fps

---
+++ Madnight/Assets/Scripts/FlashlightController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightController : MonoBehaviour {
+
+    public Light flashLight;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.F))
+            flashLight.enabled = !flashLight.enabled;
+    }
+}
---
+++ Madnight/Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour {
+
+    public float interactDistance = 5f; 
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, interactDistance))
+            {
+                if (hit.collider.CompareTag("door"))
+                {
+                    hit.collider.GetComponent<DoorOpen>().ChangeDoorState();
+                }
+            }
+
+        }
+	}
+}
